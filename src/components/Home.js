6d2a2e4d0810874ba5614fc4f2663e42e83bb5dd
Andy Nguyen <andyng1997@outlook.com>
2022-10-25 19:57:54 -0400

    node module unignore

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,20 @@
 import React from "react";
-import { BrowserRouter, Route } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {userLogin} from '../api';
 
 const homePage = () => {
     return (
         <div>
             <h1>Welcome to Stranger Things! This is your one stop shop to buy or sell all used items!</h1>
-            <BrowserRouter>
             <button>
-                <Route path='/login'>Login</Route>
+                <Link to='/login'>Login</Link>
             </button>
-            </BrowserRouter>
                 <h2>Don't have an account yet?</h2>
-            <BrowserRouter>
             <button>
-            <Route path='/register'>Register Here</Route>
+            <Link path='/register'>Register Here</Link>
             </button>
-            </BrowserRouter>
         </div>
     )
 }
- console.log('testing changes');
+
 export default homePage;
---
+++ src/components/Index.js
@@ -0,0 +1,2 @@
+export {default as homePage} from './Home';
+export {default as Login } from './Login';
