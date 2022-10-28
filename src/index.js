629e25dd0ef3081cb47f2a19b1fb3669b525fd84
Andy Nguyen <andyng1997@outlook.com>
2022-10-27 20:20:39 -0400

    attemping to code posts

--- src/index.js
+++ src/index.js
@@ -1,16 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Route, BrowserRouter, Routes, useNavigate } from 'react-router-dom';
 import {
     Home,
     Register,
     Login,
-
+    Posts,
 } from './components';
 
 const App = () => {
   const [token, setToken] = useState('');
   const [user, setUser] = useState({});
+
+  const navigate = useNavigate();
+
+  function logout() {
+      window.localStorage.removeItem('token');
+      setToken('');
+      setUser({});
+  }
+
+  async function fetchPosts() {
+      const results = await getPosts(token)
+      setPosts(results.data.posts);
+  }
+
+  async function getMe() {
+      const storedToken = window.localStorage.getItem('token');
+
+      if (!token) {
+          if (storedToken) {
+              setToken(storedToken);
+          }
+          return;
+      }
+
+      const results = await getUserDetails(token)
+      console.log(results)
+      if (results.success) {
+          setUser(results.data);
+      } else {
+          console.log(results.error.message);
+      }
+  }
+
+  useEffect(() => {
+    fetchPosts()
+}, [token])
+
+useEffect(() => {
+    getMe();
+}, [token])
+
  
   return (
       <div>
@@ -20,7 +61,7 @@ const App = () => {
                     element={<Home 
                     />}
                 />
-              <Route
+            <Route
                   path='/register'
                   element={<Register
                       setToken={setToken}
@@ -28,13 +69,21 @@ const App = () => {
 
                   />}
               />
-              <Route
+            <Route
                   path='/login'
                   element={<Login
                       setToken={setToken}
 
                   />}
               />
+            <Route
+                    path='/posts'
+                    element={<Posts
+                        token={token}
+                        posts={Posts}
+                        fetchPosts={fetchPosts}
+                    />}
+                />
           </Routes>
       </div>
   )
